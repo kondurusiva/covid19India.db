e47ce182b3bdbf6d2389198fd3ef2b01d57f56d2
kondurusiva <86553367+kondurusiva@users.noreply.github.com>
2021-10-22 11:37:38 +0530

    app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const app = express();
 const path = require("path");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
+const app = express();
 
 const dbPath = path.join(__dirname, "covid19India.db");
 let db = null;
@@ -24,39 +24,67 @@ initializeDBAndServer = async () => {
 };
 initializeDBAndServer();
 
+const convertStatesDBObjectToResponseObject = (DBObject) => {
+  return {
+    stateId: DBObject.state_id,
+    stateName: DBObject.state_name,
+    population: DBObject.population,
+  };
+};
+
+const convertDistrictDBObjectToResponseObject = (dbObject) => {
+  return {
+    districtId: dbObject.district_id,
+    districtName: dbObject.district_name,
+    stateId: dbObject.state_id,
+    cases: dbObject.cases,
+    cured: dbObject.cured,
+    active: dbObject.active,
+    deaths: dbObject.deaths,
+  };
+};
+
 //API1
-app.get("/states/", async (request, response) => {
-  const statesQuery = `SELECT * FROM state;`;
+app.get(`/states/`, async (request, response) => {
+  const statesQuery = `
+  SELECT 
+  * 
+  FROM 
+  state;`;
   const query = await db.all(statesQuery);
-  response.send(query);
+  response.send(
+    query.map((dbObject) => convertStatesDBObjectToResponseObject(dbObject))
+  );
 });
 
 //API2
-app.get("/states/:stateId", async (request, response) => {
+app.get("/states/:stateId/", async (request, response) => {
   const { stateId } = request.params;
-  const statesQuery = `SELECT * FROM state WHERE state_id=${stateId}`;
+  const statesQuery = `SELECT * FROM state WHERE state_id=${stateId};`;
   const query = await db.get(statesQuery);
-  response.send(query);
+  response.send(convertStatesDBObjectToResponseObject(query));
 });
 
 //API3
 app.post("/districts/", async (request, response) => {
   const { stateId, districtName, cases, cured, active, deaths } = request.body;
   const addDistrictQuery = `
-  INSERT INTO district(state_id,district_name,cases,cured,active,deaths)
+  INSERT INTO 
+    district(state_id,district_name,cases,cured,active,deaths)
   VALUES(
-      ${stateId},${districtName},${cases},${cured},${active},${deaths}
+      ${stateId},'${districtName}',${cases},${cured},${active},${deaths}
   );`;
-  const postQuery = await db.run(addDistrictQuery);
+  await db.run(addDistrictQuery);
   response.send("District Successfully Added");
 });
 
 //API4
 app.get("/districts/:districtId/", async (request, response) => {
   const { districtId } = request.params;
-  const districtQuery = `SELECT * FROM district WHERE district_id=${districtId}`;
-  const query = await db.get(districtQuery);
-  response.send(query);
+  const districtQuery = `
+  SELECT * FROM district WHERE district_id=${districtId};`;
+  const querying = await db.get(districtQuery);
+  response.send(convertDistrictDBObjectToResponseObject(querying));
 });
 
 //API5
@@ -77,16 +105,16 @@ app.put("/districts/:districtId/", async (request, response) => {
   const { districtName, stateId, cases, cured, active, deaths } = request.body;
   const updateQuery = `
     UPDATE 
-    district
+        district
     SET
-    "district_name"=${districtName},
-    "state_id"=${stateId},
-    "cases"=${cases},
-    "cured"=${cured},
-    "active"=${active},
-    "deaths"=${deaths}
+        district_name='${districtName}',
+        state_id=${stateId},
+        cases=${cases},
+        cured=${cured},
+        active=${active},
+        deaths=${deaths}
     WHERE 
-        "district_id"=${districtId};`;
+        district_id=${districtId};`;
   await db.run(updateQuery);
   response.send("District Details Updated");
 });
@@ -120,7 +148,7 @@ app.get("/districts/:districtId/details/", async (request, response) => {
   WHERE
   district_id=${districtId};`;
   const distract = await db.get(districtQuery);
-  response.send(distract);
+  response.send({ stateName: distract.state_name });
 });
 
 module.exports = app;
